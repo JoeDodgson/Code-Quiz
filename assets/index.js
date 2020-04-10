ebbaf0a2ddf10e694f657514a50c8c57d4c01f60
Joe Dodgson <Joe.Dodgson@arup.com>
2020-04-10 20:59:59 +0100

    Question and choice shuffling added. CSS separated into css file. Comments added throughout

--- assets/index.js
+++ assets/index.js
@@ -11,14 +11,9 @@ var questions = [
       answer: "parentheses"
     },
     {
-      title: "Arrays in JavaScript can be used to store ____.",
-      choices: [
-        "numbers and strings",
-        "other arrays",
-        "booleans",
-        "all of the above"
-      ],
-      answer: "all of the above"
+      title: "What does CSS stand for?",
+      choices: ["Cascading Style Sheet","Conveying Sheet Styles","Common Serial Styling","Cross Style Scripting"],
+      answer: "Cascading Style Sheet"
     },
     {
       title:
@@ -27,11 +22,35 @@ var questions = [
       answer: "quotes"
     },
     {
-      title:
+        title:
         "A very useful tool used during development and debugging for printing content to the debugger is:",
-      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-      answer: "console.log"
-    }
+        choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+        answer: "console.log"
+    },
+    {
+      title:
+        "What does HTML stand for?",
+      choices: ["HyperText Media Language", "HyperText Markup Language", "Hyperspace Transfer Motion Language", "Hotmail"],
+      answer: "HyperText Markup Language"
+    },
+    {
+      title:
+        "In the Bootstrap grid layout convention, how many columns are there on a page?",
+      choices: ["6", "8", "12", "24"],
+      answer: "12"
+    },
+    {
+      title:
+        "Which of these is not a valid value of the CSS 'positioning' property?",
+      choices: ["static", "inline", "sticky", "inherit"],
+      answer: "inline"
+    },
+    {
+        title:
+          "What standards organisation created the HTML5 markup language?",
+        choices: ["Massachusetts Institute of Technology (MIT)", "World Wide Web Consortium (W3C)", "Defense Advanced Research Projects Agency (DARPA)", "The Internet Control Group (ICG)"],
+        answer: "World Wide Web Consortium (W3C)"
+      }
 ];
 
 //Define the variables
@@ -52,9 +71,11 @@ var userTotalScore = document.querySelector("#user-total-score");
 var correctScoreContainer = document.querySelector("#correct-score-container");
 var timeScoreContainer = document.querySelector("#time-score-container");
 var totalScoreContainer = document.querySelector("#total-score-container");
+var enterAndSubmitContainer = document.querySelector("#enter-and-submit");
 var questionText = document.querySelector("#question");
 var nameInput = document.querySelector("#name-input");
 var currentPage = document.location.href;
+var shuffledQuestions = [];
 var username;
 var secondsLeft;
 var interval;
@@ -66,21 +87,45 @@ var oldScores;
 var today;
 var allScores;
 
-getAllScores();
-
+// Function which pulls the scores from the local storage
 function getAllScores() {
+    // If the allScores array is empty, then initialise it as an empty array
     if(localStorage.getItem("allScores") == ""){
         allScores = [];
-    }else{
+    }
+    else{
         allScores = JSON.parse(localStorage.getItem("allScores"));
     }
 }
 
-//Clicking the start button activates the start quiz function
+// Upon loading the page, pull the scores from the local storage
+getAllScores();
+
+// Randomly shuffle the questions and answers
+function shuffleQuestions(questions){
+    var i, j, k, l, x, y;
+    var shuffledQuestions = questions;
+    for (i = shuffledQuestions.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = shuffledQuestions[i];
+        shuffledQuestions[i] = shuffledQuestions[j];
+        shuffledQuestions[j] = x;
+        for (k = shuffledQuestions[i].choices.length - 1; k > 0; k--){
+            l = Math.floor(Math.random() * (k + 1));
+            y = shuffledQuestions[i].choices[k];
+            shuffledQuestions[i].choices[k] = shuffledQuestions[i].choices[l];
+            shuffledQuestions[i].choices[l] = y;
+        }
+    }   
+    return shuffledQuestions;
+}
+
+// Clicking the start button activates the start quiz function
 startBtn.addEventListener("click", startQuiz);
 
-//Start quiz function starts the timer and shows the questions
+// Start quiz function starts the timer and shows the questions
 function startQuiz(){
+    shuffledQuestions = shuffleQuestions(questions);
     startTimer();
     intro.setAttribute("style","display:none;");
     countdownContainer.setAttribute("style","display:block;");
@@ -91,9 +136,9 @@ function startQuiz(){
     //Set the first question and answer
 };
 
-//Start timer function sets the time to 60 and begins the countdown
+// Start timer function sets the time to 60 and begins the countdown
 function startTimer(){
-    secondsLeft = 60;
+    secondsLeft = 5;
     interval = setInterval(function() {
         secondsLeft--;
         renderTime();
@@ -105,27 +150,56 @@ function startTimer(){
     }, 1000);
 }
 
+// Function which is triggered by the seconds left reaching 0 or reaching the end of the questions
 function endQuiz(){
+
+    // Hide the questions and choices
     questionsChoices.setAttribute("style","display:none;"); 
+
+    // Display the score summary
     submitScore.setAttribute("style","display:block;");
+
+    // Hide the countdown
     countdownContainer.setAttribute("style","display:none;");
+
+    // Timestamp of when the user finished the quiz
     today = new Date();
+
+    // Turn the timestamp of finishing the quiz into a string
     dateOfEntry = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + " on " + today.getDate() + "-" + today.getMonth() + "-" + today.getFullYear()
-    timeScore = Math.floor( secondsLeft / 5);
     
+    // If the time score was not assigned (i.e. seconds left reached 0) then set timeScore to 0
+    if(typeof timeScore !== "num"){
+        timeScore = 0;
+    }
+
+    // Add that to the correct score to give the total score
     totalScore = correctScore + timeScore;
-    userCorrectScore.textContent = correctScore;
+    
+    // Populate the score fields
     userTimeScore.textContent = timeScore;
+    userCorrectScore.textContent = correctScore;
     userTotalScore.textContent = totalScore;
 
+    // Display the breakdown of the scores (correct score, time bonus and total score) at 1 seconds interval
     setTimeout(function(){
-        correctScoreContainer.setAttribute("style","display:block; margin-top:30px; width:100%;");
-
+        correctScoreContainer.setAttribute("class","score-breakdown row;");
+        correctScoreContainer.setAttribute("style","");
+        
         setTimeout(function(){
-            timeScoreContainer.setAttribute("style","display:block; width:100%;");
-
+            timeScoreContainer.setAttribute("class","score-breakdown row;");
+            timeScoreContainer.setAttribute("style","");
+            
             setTimeout(function(){
-                totalScoreContainer.setAttribute("style","display:block; width:100%;");
+                totalScoreContainer.setAttribute("class","score-breakdown row;");
+                totalScoreContainer.setAttribute("style","margin-bottom: 30px;");
+                
+                setTimeout(function(){
+                    enterAndSubmitContainer.setAttribute("class","score-breakdown row;");
+                    enterAndSubmitContainer.setAttribute("style","");
+                    }, 1000
+                    ); 
+
             }, 1000
             ); 
 
@@ -134,18 +208,20 @@ function endQuiz(){
 
     }, 1000
     ); 
-
-
 }
 
+// Shows the number of seconds remaining in the top right hand corner of the page
 function renderTime(){
     countdownDisplay.textContent = secondsLeft;
 }
 
+// Shows the nexy question and choices from the shuffled questions array
 function renderQuestion(){
+
     //Populate the question text
-    questionText.textContent = questions[questionIndex].title;
+    questionText.textContent = shuffledQuestions[questionIndex].title;
     choicesChild = choices.firstElementChild;
+
     //Clear out the previous question's choices
     while(choicesChild) {
         choices.removeChild(choicesChild);
@@ -153,68 +229,97 @@ function renderQuestion(){
     }
     
     //Populate this question's choices
-    for(var i=0; i < questions[questionIndex].choices.length; i++){
+    for(var i=0; i < shuffledQuestions[questionIndex].choices.length; i++){
         var newLi = document.createElement("li");
-        newLi.textContent = questions[questionIndex].choices[i];
+        newLi.textContent = shuffledQuestions[questionIndex].choices[i];
         choices.appendChild(newLi);
     }
 }
 
+// Event listener triggers depending on which choice the user clicks on
 choices.addEventListener("click",function(){
-    if(event.target.textContent === questions[questionIndex].answer){
+
+    // If the user clicks the correct answer, change color to green and add 1 to the correct score
+    if(event.target.textContent === shuffledQuestions[questionIndex].answer){
         event.target.setAttribute("style","background-color: #00e300") ;
         correctScore++;
     }
+    // If the user clicks an incorrect answer, change color to red and subtract 5 seconds from the time left
     else{
         event.target.setAttribute("style","background-color: #e30000") ;
         secondsLeft -= 5;
     }
-    //Allow the user to see the background-colour for a fraction of a seconds before displaying the next question
+
+    // Allow the user to see the background-colour for a fraction of a seconds before displaying the next question
     setTimeout(function(){
-        if (questionIndex === questions.length - 1) {
-            endQuiz();
+
+        // If this is the final questions in the array, then pause for a fraction of the second
+        if (questionIndex === shuffledQuestions.length - 1) {
+            setTimeout(function(){
+            }, 200);
+
+            // Then hide the questions and choices from display
+            questionsChoices.setAttribute("style","display:none;");
+
+            // Calculate the time bonus score - divide remaining seconds by 10 and round down
+            timeScore = Math.floor( secondsLeft / 10);
+
+            // Set the seconds left to 0 to trigger the endQuiz function
+            secondsLeft = 0;
         }
+
+        // Move onto the next question and display the next question
         else {
-            questionIndex++;
+            questionIndex ++;
             renderQuestion();
         }
     }, 200
     );
 })
 
+// Add an event listener for the Submit button
 submitBtn.addEventListener("click",function(){
+
+    // Prevent default actions from happening - e.g. page flickering
     event.preventDefault();
-    //If the content of the entry field is blank, alert the user
+
+    // If the content of the entry field is blank, alert the user
     if(nameInput.value == ""){
         alert("You must enter a name to submit a score");
     }
-    //Store the user's name and score in local storage, then redirect to the Highscores page
+    // Store the user's name and score in local storage, then redirect to the Highscores page
     else{
         username = nameInput.value;
-        //Update the scores array and update the scores displayed in scores.html
+
+        // Update the scores array and update the scores displayed in scores.html
         updateScores();
-        //Redirect user to the Highscores page
+
+        // Redirect user to the Highscores page
         window.location.href = "./scores.html";
     }
-}
-)
+});
 
+// Define the function which adds the user's score to the allScores array
 function updateScores() {
  
-    //Then insert the new score into the allScores array
+    // Insert the new score into the allScores array
+    // If the allScores array is empty, set the 0 index item of the array to the user's score
     if(allScores.length === 0 && allScores[0] == ""){
         allScores[0] = {
             name: username,
             score: totalScore,
             date: dateOfEntry,
         }
-    }else{
+    }
+    // Else push the user's score into the end of the array
+    else{
         allScores.push({
             name: username,
             score: totalScore,
             date: dateOfEntry,
-        })
+        });
     }
-    //Then store the updated allScores array into the local storage
+
+    // Then store the updated allScores array into the local storage
     localStorage.setItem("allScores",JSON.stringify(allScores));
 }
--- assets/scores.js
+++ assets/scores.js
@@ -1,4 +1,3 @@
-//Define the quiz array
 //Define the variables
 var clearBtn = document.querySelector("#clear-button");
 var scoresDisplay = document.querySelector("#scores-display");
@@ -7,22 +6,20 @@ var scoresChild;
 var currentPage = document.location.href;
 var allScores;
 
-//Add an event for on load to update the scores
-
-function getAllScores() {
-  if(localStorage.getItem("allScores") == ""){
-      allScores = [];
-  }else{
-      allScores = JSON.parse(localStorage.getItem("allScores"));
-  }
-}
+// On load, display all scores from the local storage
+renderScores();
 
 function renderScores() {
+
+  // Pulls the scores from the local storage
   getAllScores();
+  
+  // If there are scores in the allScores array, display the highscores table
   if(allScores.length > 0){
     scoresTable.removeAttribute("class");
-    scoresTable.setAttribute("class","table table-responsive");
+    scoresTable.setAttribute("class","table table-responsive table-striped");
     
+    // Sort the entries in the allScores array by "score"
     var data = allScores.sort(compareValues("score", "desc"));
     
     //Display all the updated scores from the allScores array
@@ -41,43 +38,60 @@ function renderScores() {
       
     }
   }   
+  
+  // If the allScores array is empty, do not display the highscores table
   else{
     scoresTable.setAttribute("class","d-none");
   }
-
+  
 }
 
-renderScores();
+// Pulls the scores from the local storage
+function getAllScores() {
+
+  // If the allScores array is not initialised, initialise it with an empty array
+  if(localStorage.getItem("allScores") == ""){
+    allScores = [];
+  }
+  else{
+    allScores = JSON.parse(localStorage.getItem("allScores"));
+  }
+}
 
 
-function compareValues(key, order = 'asc') {
-    return function innerSort(a, b) {
-      if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
-        // property doesn't exist on either object
-        return 0;
-      }
-  
-      const varA = (typeof a[key] === 'string')
-        ? a[key].toUpperCase() : a[key];
-      const varB = (typeof b[key] === 'string')
-        ? b[key].toUpperCase() : b[key];
-  
-      let comparison = 0;
-      if (varA > varB) {
-        comparison = 1;
-      } else if (varA < varB) {
-        comparison = -1;
-      }
-      return (
-        (order === 'desc') ? (comparison * -1) : comparison
+// Function to sort the entries in the allScores array
+function compareValues(key, order = "asc") {
+  return function innerSort(a, b) {
+    if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+      return 0;
+    }
+    
+    const varA = (typeof a[key] === "string")
+    ? a[key].toUpperCase() : a[key];
+    const varB = (typeof b[key] === "string")
+    ? b[key].toUpperCase() : b[key];
+    
+    let comparison = 0;
+    if (varA > varB) {
+      comparison = 1;
+    } else if (varA < varB) {
+      comparison = -1;
+    }
+    return (
+      (order === "desc") ? (comparison * -1) : comparison
       );
-    };
+  };
+}
+  
+// Add event listener for the clear Highscores button
+clearBtn.addEventListener("click", function(){
+
+  // Ask the user to confirm they want to clear the highscores before proceeding
+  var confirmClear = confirm("Are you sure you want to clear the Highscores?\nOk = Yes. Cancel = No")
+  
+  if (confirmClear){
+    localStorage.setItem("allScores","");
+    renderScores();
   }
 
-  clearBtn.addEventListener("click", function(){
-    var confirmClear = confirm("Are you sure you want to clear the Highscores?\nOk = Yes. Cancel = No")
-    if (confirmClear){
-      localStorage.setItem("allScores","");
-      renderScores();
-    }
-  })
+})
